358ec460d911d5c21930140766e83e149c6d3dba
Danya <danya9104449383@gmail.com>
2019-02-28 20:58:15 +0300

    Отрисовка карты по символьнму шаблону.

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
 from pygame import *
 
-if __name__ == '__main__':
-    init()
-    screen = display.set_mode((640, 480))
-    timer = time.Clock()
+import maps
 
-    while True:
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+WALL_LENGTH = 32
+WALL_COLOR = "#662218"
+
+
+class MainScene:
+
+    def __init__(self):
+        init()
+        self.screen = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        display.set_caption("Ghost&Hunter")
+        self.timer = time.Clock()
+
+    def check_events(self):
         for e in event.get():
             if e.type == QUIT:
                 raise SystemExit
-        timer.tick(60)
-        display.update()
+
+    def draw_map(self):
+        x = y = 0
+        for row in maps.hotel_map:
+            for col in row:
+                if col == '-':
+                    wall = Surface((WALL_LENGTH, WALL_LENGTH))
+                    wall.fill(Color(WALL_COLOR))
+                    self.screen.blit(wall, (x, y))
+                x += WALL_LENGTH
+            y += WALL_LENGTH
+            x = 0
+
+    def mainloop(self):
+        while True:
+            self.check_events()
+            self.draw_map()
+            self.timer.tick(60)
+            display.update()
+
+
+if __name__ == '__main__':
+    game = MainScene()
+    game.mainloop()

8e9ba3720f70979bc375d7b38f86b2cbdfeb8633
Danya0x07 <dlef0xf8@gmail.com>
2020-06-19 12:12:08 +0300

    Добавлена лицензия GPLv3.

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-# -*- coding: utf-8 -*-
+# Copyright (C) 2019-2020, Daniel Efimenko
+#
+# This file is part of Haunted_Library.
+#
+# Haunted_Library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Haunted_Library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Haunted_Library.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 """Игра про охоту на привидений.
 
 Шутер от 3 лица, вид сверху.
@@ -17,13 +34,13 @@ from scenes.gamover_scene import GameOverScene
 from utils.config import *
 
 
-__author__ = "Daniil Efimenko"
+__author__ = "Daniel Efimenko"
 __copyright__ = "Copyright 2019, The Haunted_Library project"
-__credits__ = ["Daniil Efimenko", "Alexander Isaev", "Maxim Babenko"]
+__credits__ = ["Daniel Efimenko", "Alexander Isaev", "Maxim Babenko"]
 __license__ = "GPL"
 __version__ = "1.1.0"
-__maintainer__ = "Daniil Efimenko"
-__email__ = "danya9104449383@gmail.com"
+__maintainer__ = "Daniel Efimenko"
+__email__ = "dlef0xf8@gmail.com"
 __status__ = "Production"
 
 
